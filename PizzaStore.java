0d54588ea33654cfa7c02df237e5e228310157a5
nickkunta <nickson_kv@hotmail.com>
2015-11-18 14:28:30 +0700

    Update PizzaStore.java

--- PizzaStore.java
+++ PizzaStore.java
@@ -1,8 +1,4 @@
 public class PizzaStore{
-	//PizzaFactory pFactory;
-	// public PizzaStore(PizzaFactory inputFactory){
-	// 	this.pFactory = inputFactory;
-	// }
 	public Pizza orderPizza(String pizType){
 		Pizza p;
 		p = PizzaFactory.createPizza(pizType);
